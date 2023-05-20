856d6660f4844a3410848bd06f4c69621698c9d9
emretantu <emretantu@gmail.com>
2023-05-20 06:28:04 +0300

    some changes on general layout

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -1,11 +1,12 @@
 import classes from "./Aside.module.css";
+import { Link } from "react-router-dom"
 
 const Aside = () => {
   return (
     <>
       <aside>
         <section className={classes.brand}>
-          
+          <Link to="/" className={classes["brand__link"]}>PMSystem</Link>
         </section>
 
         <section className={classes["portfolios-list"]}>
