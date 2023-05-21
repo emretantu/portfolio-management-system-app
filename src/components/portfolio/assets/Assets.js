f1bf65aeee614dbdac3814ba6882ecbb326a9d92
emretantu <emretantu@gmail.com>
2023-05-21 07:33:57 +0300

    Assets component added

---
+++ src/components/portfolio/assets/Assets.js
@@ -0,0 +1,16 @@
+import classes from "./Assets.module.css";
+
+const Assets = () => {
+  return (
+    <div className={classes.assets}>
+      <section className={classes.head}>
+        
+      </section>
+      <section className={classes.list}>
+
+      </section>
+    </div>
+  );
+}
+
+export default Assets;
