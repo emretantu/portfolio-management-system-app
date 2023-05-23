4e5a4d09384b020f76a8e17dc1488246b5a9e282
emretantu <emretantu@gmail.com>
2023-05-23 15:08:07 +0300

    the heights of the some components updated

--- src/components/portfolios/PortfoliosList.js
+++ src/components/portfolios/PortfoliosList.js
@@ -4,7 +4,25 @@ import classes from "./PortfoliosList.module.css";
 const data = [
   {pname: "Ana Portföy", marketValue: "100", to:"/test1"},
   {pname: "Fonlar", marketValue: "400", to:"/test2"},
-  {pname: "Hisse Senedi", marketValue: "1100", to:"/test3"}
+  {pname: "Hisse Senedi", marketValue: "1100", to:"/test3"},
+  {pname: "Ana Portföy", marketValue: "100", to:"/test1"},
+  {pname: "Fonlar", marketValue: "400", to:"/test2"},
+  {pname: "Hisse Senedi", marketValue: "1100", to:"/test3"},
+  {pname: "Ana Portföy", marketValue: "100", to:"/test1"},
+  {pname: "Fonlar", marketValue: "400", to:"/test2"},
+  {pname: "Hisse Senedi", marketValue: "1100", to:"/test3"},
+  {pname: "Ana Portföy", marketValue: "100", to:"/test1"},
+  {pname: "Fonlar", marketValue: "400", to:"/test2"},
+  {pname: "Hisse Senedi", marketValue: "1100", to:"/test3"},
+  {pname: "Ana Portföy", marketValue: "100", to:"/test1"},
+  {pname: "Fonlar", marketValue: "400", to:"/test2"},
+  {pname: "Hisse Senedi", marketValue: "1100", to:"/test3"},
+  {pname: "Ana Portföy", marketValue: "100", to:"/test1"},
+  {pname: "Fonlar", marketValue: "400", to:"/test2"},
+  {pname: "Hisse Senedi", marketValue: "1100", to:"/test3"},
+  {pname: "Ana Portföy", marketValue: "100", to:"/test1"},
+  {pname: "Fonlar", marketValue: "400", to:"/test2"},
+  {pname: "Hisse Senedi", marketValue: "1100", to:"/test3"},
 ]
 
 const PortfoliosList = () => {
@@ -15,10 +33,10 @@ const PortfoliosList = () => {
         data.map((d) => {
           return (
             <PortfolioLink
-            pname={d.pname}
-            marketValue={d.marketValue}
-            to={d.to}
-            key={d.to}
+              pname={d.pname}
+              marketValue={d.marketValue}
+              to={d.to}
+              key={d.to}
           />
           );
         })
